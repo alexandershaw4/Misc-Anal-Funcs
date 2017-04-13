72bccd32bcfbef8a46f9f4677bbca5ac74d5a324
alex <alexandershaw4@gmail.com>
2017-04-13 08:48:28 +0100

    Added 3D filtering

--- NewMeanFilt.m
+++ NewMeanFilt.m
@@ -1,13 +1,14 @@
-function y = NewMeanFilt(mIn,n,m)
+function y = NewMeanFilt3D(mIn,n,m)
 % Mean window filtering & matrix sampling
 % mIn in an n dimensional matrix
 % n   is a resampling value (1 = no resamp, .5 = half, 2 = double etc).
 % m   is the window kernel size
 %
-% AS2016 [updt]
+% *uses imresize which only resizes the first 2 dimensions!*
+% AS2016 [updt] [updt 17]
 
 
-if ndims(mIn) > 2; 
+if ndims(mIn) > 3; 
     % handle high dimentions
     mOut = size(mIn);
     mIn  = VecRetainDim(mIn,1);
@@ -15,7 +16,7 @@ end
 
 
 M = imresize(mIn,n);
-y = smoothmat(M,m,m);
+y = smoothmat(M,m,m,m);
 
 
 try mOut;
@@ -25,24 +26,25 @@ end
 
 end
 
-function matrixOut = smoothmat(matrixIn,Nr,Nc)
+function matrixOut = smoothmat(matrixIn,Nr,Nc,Nz)
 % Smooths 2D array data.  Ignores NaN's.
-if nargin < 2, error('Not enough input arguments!'), end
 
-N(1) = Nr; 
-N(2) = Nc;
 
 if length(N(1)) ~= 1, error('Nr must be a scalar!'), end
 if length(N(2)) ~= 1, error('Nc must be a scalar!'), end
+if length(N(3)) ~= 1, error('Nz must be a scalar!'), end
 
-[row,col] = size(matrixIn);
+[row,col,dep] = size(matrixIn);
+
 eL = spdiags(ones(row,2*N(1)+1),(-N(1):N(1)),row,row);
 eR = spdiags(ones(col,2*N(2)+1),(-N(2):N(2)),col,col);
+eZ = spdiags(ones(dep,2*N(3)+1),(-N(3):N(3)),dep,dep);
 
 A = isnan(matrixIn);
 matrixIn(A) = 0;
 
-nrmlize = eL*(~A)*eR;
+%nrmlize = eL*(~A)*eR;
+nrmlize = eL*(~A)*eR*(~A)*eZ;
 nrmlize(A) = NaN;
 
 matrixOut = eL*matrixIn*eR;
